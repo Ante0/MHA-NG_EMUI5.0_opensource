bd13baccd0ed8adac55a1bff44bf80b692408bf4
Andreas Tillman <ante00@gmail.com>
2017-06-07 19:00:03 +0200

    Added conservativex governor

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -506,6 +506,9 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
 extern struct cpufreq_governor cpufreq_gov_sched;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVEX)
+extern struct cpufreq_governor cpufreq_gov_conservativeX;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservativex)
 #endif
 
 /*********************************************************************
