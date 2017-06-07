14b8971bb3c5e002acc7bc459ead8f8f99416bba
Andreas Tillman <ante00@gmail.com>
2017-06-07 20:21:47 +0200

    Added OndemandPlus governor

--- kernel/include/linux/cpufreq.h
+++ kernel/include/linux/cpufreq.h
@@ -512,6 +512,9 @@ extern struct cpufreq_governor cpufreq_gov_conservativex;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
 extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
+extern struct cpufreq_governor cpufreq_gov_ondemandplus;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
 #endif
 
 /*********************************************************************
