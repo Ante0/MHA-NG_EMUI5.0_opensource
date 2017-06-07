e9e7b40798dab741c54b4dc02b354b35d1291444
Andreas Tillman <ante00@gmail.com>
2017-06-07 18:40:29 +0200

    fix

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ~/Android/Kernel/Huawei/hi3660/
-rm -rf out
-mkdir out
-cd ~/Android/Kernel/Huawei/hi3660/kernel
-make mrproper
-make CROSS_COMPILE=aarch64-linux-android- ARCH=arm64 O=~/Android/Kernel/Huawei/hi3660/out merge_hi3660_defconfig
-make CROSS_COMPILE=aarch64-linux-android- ARCH=arm64 O=~/Android/Kernel/Huawei/hi3660/out kconfig
-make CROSS_COMPILE=aarch64-linux-android- ARCH=arm64 O=~/Android/Kernel/Huawei/hi3660/out -j8
